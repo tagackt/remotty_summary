0bf6516cfc6d3149afe92a620051510d54d9144b
taga <taga@321sa.net>
2015-06-05 02:24:48 +0900

    認証して、ユーザ情報出力

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+        user.name = auth['info']['name'] || ""
+        user.room_id = auth['info']['room_id'] || ""
+      end
+    end
+  end
+
+  def update_credentials(oauth_data)
+    self.update_column(:token, oauth_data.credentials.token)
+  end
+
+end
---
+++ db/migrate/20150604171438_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :uid, :string
+    add_column :users, :provider, :string
+    add_column :users, :token, :string
+    add_column :users, :room_id, :string
+  end
+end
